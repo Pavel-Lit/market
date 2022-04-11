ef6d666b7d35a8e9064f3d1f6f977542a4e79e58
Pavel-Lit <slipknot4next@gmail.com>
2022-04-11 11:03:16 +0300

    lesson 2
    homework

--- march-market/src/main/java/ru/geekbrains/marchmarket/controllers/CartController.java
+++ march-market/src/main/java/ru/geekbrains/marchmarket/controllers/CartController.java
@@ -1,36 +1,50 @@
 package ru.geekbrains.marchmarket.controllers;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-import ru.geekbrains.marchmarket.entities.Product;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.geekbrains.marchmarket.converters.CartConverter;
+import ru.geekbrains.marchmarket.dtos.CartDto;
 import ru.geekbrains.marchmarket.services.CartService;
-import ru.geekbrains.marchmarket.services.ProductService;
+import ru.geekbrains.marchmarket.utils.Cart;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.math.BigDecimal;
 
 @RestController
-@RequiredArgsConstructor
 @RequestMapping("/api/v1/cart")
+@RequiredArgsConstructor
 public class CartController {
-
     private final CartService cartService;
+    private final CartConverter cartConverter;
 
+    @GetMapping("/old-version")
+    public Cart getCurrentCart() {
+        return cartService.getCurrentCart();
+
+    }
 
     @GetMapping("")
-    public List<Product> allCart() {
-        return cartService.getAllCart();
+    public CartDto getCurrentCartDto(Cart cart) {
+        return cartConverter.getCartDto(cartService.getCurrentCart());
     }
 
 
-    @GetMapping ("/{id}")
-    public void addToCart(@PathVariable Long id){
-
-        cartService.addToCart(id);
-
+    @GetMapping("/add/{productId}")
+    public void addProductToCart(@PathVariable Long productId) {
+        cartService.addToCart(productId);
     }
 
+    @GetMapping("/remove/{id}")
+    public void removeOneProductFromCart(@PathVariable Long id) {
+        cartService.removeById(id);
+    }
 
+    @GetMapping("/clear")
+    public void clearCart() {
+        cartService.clearCart();
+
+    }
 
 }
